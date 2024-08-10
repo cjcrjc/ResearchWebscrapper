8374d133b5bccf02492a89b15881f386de075444
Cameron Chin <cjchin@uwaterloo.ca>
2024-08-09 20:50:06 -0400

    Significant scrape runtime improvements

--- datacollection/webscrapper.py
+++ datacollection/webscrapper.py
@@ -1,33 +1,49 @@
 from bs4 import BeautifulSoup
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
-import PySimpleGUI as sg
-from multiprocessing import Process
-import requests, sys, os, platform
+import requests, sys, os
+from PIL import Image
+from io import BytesIO
+from concurrent.futures import ThreadPoolExecutor, as_completed
+import time
+from itertools import repeat
 
 # Get the path of the current directory
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
-# Define a function to scrape scientific articles
-def scrape_page(url_data, download_folder, base_site, article_selector, pdf_container_selector):
-    i = 0
-    html_text = url_data
+def is_mostly_white(img, threshold=0.7):
+    # Convert image to RGB
+    img = img.convert("RGB")
+    
+    # Get image dimensions
+    width, height = img.size
+    total_pixels = width * height
+    
+    # Initialize white pixel counter
+    white_pixels = 0
+    
+    # Define what counts as "white"
+    white_threshold = (250, 250, 250)  # RGB values
 
-    # Iterate through the articles on the current page
+    # Count the number of white pixels
+    for pixel in img.getdata():
+        if pixel >= white_threshold:
+            white_pixels += 1
+
+    # Calculate the ratio of white pixels
+    white_ratio = white_pixels / total_pixels
+
+    # Check if the ratio exceeds the threshold
+    return white_ratio > threshold
+
+def scrape_page(html_text, download_folder, base_site, article_selector, image_container_selector):
     for art_link in get_articles(html_text, base_site, article_selector):
-        i += 1
         art_name = get_article_name(art_link)
-        print("[+]", i, art_name)
-        download_pdf(art_link, art_name, base_site, download_folder, pdf_container_selector)
+        download_image(art_link, art_name, download_folder, image_container_selector)
 
-# Function to retrieve HTML data from a URL using requests and BeautifulSoup
 def get_data(url):
     data = requests.get(url)
     html_text = BeautifulSoup(data.text, 'html.parser')
     return html_text
 
-# Function to get the URL of the next page of results
 def get_next_page(html_text, next_page_selector):
     pagination = html_text.find(*next_page_selector[0])
     if not pagination:
@@ -35,81 +51,82 @@ def get_next_page(html_text, next_page_selector):
     next_page_link = pagination.find(*next_page_selector[1])
     if not next_page_link:
         return None
-
     return str(next_page_link['href'])
 
-# Function to extract article links from the current page
 def get_articles(html_text, base_site, article_selector):
     articles_list = html_text.find(*article_selector[0])
     articles = articles_list.find_all(*article_selector[1])
-
     article_links = [article.find('a', {"class": "c-card__link u-link-inherit"})['href'] for article in articles]
     article_links = [link for link in article_links if link is not None]
     final_article_links = [base_site + link for link in article_links]
-
     return final_article_links
 
-# Function to get the name of an article from its link
 def get_article_name(art_link):
     page_data = get_data(art_link)
     art_name = str(page_data.h1.contents[0]).replace("<i>", "").replace("</i>", "").replace("/", " ").replace("-", "")
     return art_name
 
-# Function to download a PDF article
-def download_pdf(art_link, art_name, base_site, download_folder, pdf_container_selector):
-    page_data = get_data(art_link)
-    download_button_div = page_data.find(*pdf_container_selector[0])
-    if download_button_div and art_name is not None:
-        download_button_link = download_button_div.find(*pdf_container_selector[1])['href']
-        res = requests.get(base_site + download_button_link)
-
-        if res.status_code == 200 and art_name is not None:
-            pdf_path = os.path.join(download_folder, f"{art_name[:25]}.pdf")
-            # Make a legal filename
-            illegal_characters = ["#","%","&","{","}","\\","<",">","*","$","!","'",'"',":","@","+","`","|","="]
-            for char in illegal_characters:
-                if char in pdf_path:
-                    pdf_path.replace(char, " ")
-
-            with open(pdf_path, 'wb') as pdf:
-                pdf.write(res.content)
-        else:
-            print("FAILED TO DOWNLOAD")
-
-# Function to perform a search and return the search results URL using Selenium
-def perform_search(search_term):
-    search_term = search_term.strip().replace(" ", "+")
-    search_results_url = f"https://www.nature.com/search?q={search_term}&journal="
+def get_image(link, download_folder, art_name):
+    global i
+    res = requests.get(link)
+    image_extension = os.path.splitext(link)[1]
+    if image_extension == '' or image_extension == '.svg':
+        raise(ValueError())
+    illegal_characters = ["#","%","&","{","}","\\","<",">","*","$","!","'",'"',":","@","+","`","|","="]
+    image_name = os.path.basename(art_name[:20]).translate(str.maketrans({char: " " for char in illegal_characters}))
     
-    return search_results_url
+    image_path = os.path.join(download_folder, f"{image_name}{i}{image_extension}")
+    
+    if len(res.content) < 1000:
+        raise(ValueError())
+    
+    with open(image_path, 'wb') as image_file:
+        img = Image.open(BytesIO(res.content))
+        if is_mostly_white(img) or img.size[0] < 200 or img.size[1] < 200:
+            raise(ValueError())
+        img.close()
+        image_file.write(res.content)
+        i += 1
+    return res
 
-# Main Logic and Parallelization
+def download_image(art_link, art_name, download_folder, image_container_selector):
+    global i
+    i = 0
+    page_data = get_data(art_link)
+    if art_name is not None:
+        image_objs = page_data.find_all(image_container_selector)
+        image_links = [obj['src'] for obj in image_objs]
+        links = []
+        for i, link in enumerate(image_links):
+            if not link.startswith('http'):
+                links.append('https:' + link)
+            else:
+                links.append(link)
+        with ThreadPoolExecutor() as executor2:
+            executor2.map(get_image, links, repeat(download_folder), repeat(art_name))
+
 def scrape():
-    # Define journal-specific selectors (modify as needed)
     nature_article_selector = [['ul', {"class": "app-article-list-row"}], ['li', {"class": "app-article-list-row__item"}]]
     nature_next_page_selector = [['li', {"class":"c-pagination__item", "data-page":"next"}], ['a', {"class": "c-pagination__link"}]]
-    nature_pdf_container_selector = [['div', {"class": "c-pdf-container"}] , ['a', {"class": "u-button u-button--full-width u-button--primary u-justify-content-space-between c-pdf-download__link"}]]
+    nature_image_container_selector = ['img']
 
-    download_folder = dir_path + '/downloaded'
+    download_folder = dir_path + '/images'
     base_site = 'https://www.nature.com'
-    # Create the download folder if it doesn't exist
+    
     if not os.path.exists(download_folder):
         os.mkdir(download_folder)
 
-    # Check if a search term is provided as a command-line argument, otherwise prompt the user
     search_term = sys.argv[1] if len(sys.argv) == 2 else None
     if not search_term:
-        search_term = sg.popup_get_text("Enter Search Term:", title="Webscraper Search Term")
-    if not search_term:
+        search_term = input("Enter Search Term: ")
+    if search_term == '':
         raise SystemExit()
 
-    # Perform the initial search and get the results URL
-    url = perform_search(search_term)
+    url = f"https://www.nature.com/search?q={search_term}&journal="
     urls = [base_site + url]
     while url:
         print(f"[+] Added to scrape list: {url}")
         if len(urls) == 1:
-            # Need to store data from the first iteration otherwise nature cause nature won't allow another request
             firstpagedata = get_data(url)
             url = get_next_page(firstpagedata, nature_next_page_selector)
         else:
@@ -117,24 +134,22 @@ def scrape():
         if url:
             url = base_site + url
             urls.append(url)
-            if len(urls) == 20: # Max number of pages that Nature will let you observe
+            if len(urls) == 20:
                 break
 
-    processes = []
-    args_list = [(get_data(url), download_folder, base_site, nature_article_selector, nature_pdf_container_selector) for url in urls[1:]]
-    args_list.insert(0, (firstpagedata, download_folder, base_site, nature_article_selector, nature_pdf_container_selector))
+    args_list = [(get_data(url), download_folder, base_site, nature_article_selector, nature_image_container_selector) for url in urls[1:]]
+    args_list.insert(0, (firstpagedata, download_folder, base_site, nature_article_selector, nature_image_container_selector))
+    with ThreadPoolExecutor() as executor:
+        futures = [executor.submit(scrape_page, *args) for args in args_list]
+        for future in as_completed(futures):
+            try:
+                future.result()
+            except Exception as e:
+                print(f"Error occurred: {e}")
 
-    # Parallelization based on the platform
-    if platform.system() == 'Linux':
-        for i in range(len(urls)):
-            scrape_page(*args_list[i])
-    else:
-        for i in range(len(urls)):
-            p = Process(target=scrape_page, args= args_list[i])
-            p.start()
-            processes.append(p)
+    print("FINISHED SCRAPING")
 
-        for process in processes:
-            process.join()
-
-    print("FINISHED SCRAPPING")
+if __name__ == "__main__":
+    start = time.time()
+    scrape()
+    print(f"Total Time: {time.time() - start}")
--- run.py
+++ run.py
@@ -1,5 +1,4 @@
 from datacollection.webscrapper import scrape
-from datacollection.pullimages import pull_all_images
 from cnnsorter.sorteruse import run_sorter
 from rcnn.rcnnuse import rcnn_crop
 from multiprocessing import freeze_support, set_start_method
@@ -13,16 +12,13 @@ if __name__ == '__main__':
     freeze_support()
     
     #Scrape articles from nature.com
-    scrape()
-    #Pull images from downloaded articles
-    pull_all_images()
+    # scrape()
     #Primilinary binary sort of images to keep useful ones
     run_sorter()
     #RCNN pass to crop images based on predicted bounding boxes and keep only useful subimages
     rcnn_crop()
 
     #Cleanup empty directories
-    rmtree(path.join(dir_path,"datacollection","downloaded"))
     rmtree(path.join(dir_path,"datacollection","images"))
     rmtree(path.join(dir_path,"datacollection","__pycache__"))
     rmtree(path.join(dir_path,"cnnsorter","filtered"))
