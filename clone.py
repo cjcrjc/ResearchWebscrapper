e00b0a5fc70ee95f28f2fbdfd65226c7262f8672
Cameron Chin <cjchin@uwaterloo.ca>
2024-08-12 23:01:42 -0400

    just ssim check

--- clone.py
+++ clone.py
@@ -26,43 +26,18 @@ for set in os.walk(check_path):
                     new.append(os.path.join(check_path, file))
 
 for file in new:
-    mses = []
     ssims = []
-    convs = []
-    convs2 = []
-    if 'IMG_4557' in file:
-        img1 = np.array(Image.open(file).convert("L"))
-        img1 = (img1 - np.mean(img1))#[img1.shape[0]//2-size:img1.shape[0]//2+size, img1.shape[1]//2-size:img1.shape[1]//2+size]
-        for file2 in current:
-            # img2 = np.array(Image.open(file2).convert("L").resize((img1.shape[1], img1.shape[0])))
-            img2_unsized = np.array(Image.open(file2).convert("L"))
-            img2_unsized = (img2_unsized - np.mean(img2_unsized))
-            pad_width = ((0, img1.shape[0] - img2_unsized.shape[0]), (0, img1.shape[1] - img2_unsized.shape[1]))
-            if 'IMG_4557' in file2:
-                print(img1.shape, img2_unsized.shape)
-            if img1.shape[0] > img2_unsized.shape[0] and img1.shape[1] > img2_unsized.shape[1]:
-                pad_height = img1.shape[0] - img2_unsized.shape[0]
-                pad_width = img1.shape[1] - img2_unsized.shape[1]
-                img2_padded = np.pad(img2_unsized, ((0, pad_height), (0, pad_width)), mode='constant', constant_values=np.mean(img2_unsized))
-                # mses.append(mse(img1, img2))
-                ssims.append(ssim(img1, img2_padded, data_range=img1.max()-img1.min()))
-                # conv = convolve2d(img1, img2_unsized)
-                # convs.append(conv.max())
-                # convs.append(conv.min())
-                # print(conv.max(), file2)
-                if 'IMG_4557' in file2:
-                    true = ssim(img1, img2_padded, data_range=img1.max()-img1.min())
-                #     true1 = conv.max()
-                #     true2 = conv.min()
-                #     plt.imshow(conv)
-                #     plt.show()
-        break
-    # if min(ssims) < 0.01:
-    # print(max(ssims), sum(ssims)/len(ssims), file)
+    img1 = np.array(Image.open(file).convert("L"))
+    img1 = (img1 - np.mean(img1))
+    for file2 in current:
+        img2_unsized = np.array(Image.open(file2).convert("L"))
+        img2_unsized = (img2_unsized - np.mean(img2_unsized))
+        pad_width = ((0, img1.shape[0] - img2_unsized.shape[0]), (0, img1.shape[1] - img2_unsized.shape[1]))
+        if img1.shape[0] > img2_unsized.shape[0] and img1.shape[1] > img2_unsized.shape[1]:
+            pad_height = img1.shape[0] - img2_unsized.shape[0]
+            pad_width = img1.shape[1] - img2_unsized.shape[1]
+            img2_padded = np.pad(img2_unsized, ((0, pad_height), (0, pad_width)), mode='constant', constant_values=np.mean(img2_unsized))
+            ssims.append(ssim(img1, img2_padded, data_range=img1.max()-img1.min()))
 
-# if true1 == max(convs) or true1 == min(convs) or true2 == max(convs) or true2 == min(convs):
-#     print('works')
-if true == max(ssims):
-    print('works')
-plt.hist(ssims, 200)
-plt.show()
+    if max(ssims) > 0.1:
+        print(file)
