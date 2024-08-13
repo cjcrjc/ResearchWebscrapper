41680dfb7ad674e4e101d6a9ba3499646e59bad9
Cameron Chin <cjchin@uwaterloo.ca>
2024-08-12 23:07:56 -0400

    name based duplicate check

--- clone.py
+++ clone.py
@@ -16,28 +16,17 @@ for set in os.walk(all_path):
         if type(sublist) == list:
             for file in sublist:
                 if len(file) > 10:
-                    current.append(os.path.join(set[0], file))
+                    current.append(file)
 
 for set in os.walk(check_path):
     for sublist in set:
         if type(sublist) == list:
             for file in sublist:
                 if len(file) > 10:
-                    new.append(os.path.join(check_path, file))
+                    new.append(file)
 
 for file in new:
-    ssims = []
-    img1 = np.array(Image.open(file).convert("L"))
-    img1 = (img1 - np.mean(img1))
     for file2 in current:
-        img2_unsized = np.array(Image.open(file2).convert("L"))
-        img2_unsized = (img2_unsized - np.mean(img2_unsized))
-        pad_width = ((0, img1.shape[0] - img2_unsized.shape[0]), (0, img1.shape[1] - img2_unsized.shape[1]))
-        if img1.shape[0] > img2_unsized.shape[0] and img1.shape[1] > img2_unsized.shape[1]:
-            pad_height = img1.shape[0] - img2_unsized.shape[0]
-            pad_width = img1.shape[1] - img2_unsized.shape[1]
-            img2_padded = np.pad(img2_unsized, ((0, pad_height), (0, pad_width)), mode='constant', constant_values=np.mean(img2_unsized))
-            ssims.append(ssim(img1, img2_padded, data_range=img1.max()-img1.min()))
-
-    if max(ssims) > 0.1:
-        print(file)
+        if file[:10] == file2[:10]:
+            print(file)
+            break
